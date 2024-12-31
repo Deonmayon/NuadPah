1fd4b84e0e6347a1ab065b0a9bb82a8d7912a82c
martksk <krtsk2547@gmail.com>
2024-12-31 11:57:45 +0700

    change style to match figma

--- frontend/lib/login.dart
+++ frontend/lib/login.dart
@@ -1,3 +1,127 @@
+// import 'package:flutter/material.dart';
+// import 'welcome.dart';
+
+// class LoginPage extends StatefulWidget {
+//   const LoginPage({super.key});
+
+//   @override
+//   _LoginPageState createState() => _LoginPageState();
+// }
+
+// class _LoginPageState extends State<LoginPage> {
+//   final TextEditingController _usernameController = TextEditingController();
+//   final TextEditingController _passwordController = TextEditingController();
+//   String _errorMessage = '';
+
+//   void _login() {
+//     final username = _usernameController.text;
+//     final password = _passwordController.text;
+
+//     if (username == 'admin' && password == '1234') {
+//       Navigator.push(
+//         context,
+//         MaterialPageRoute(builder: (context) => WelcomePage()),
+//       );
+//     } else {
+//       setState(() {
+//         _errorMessage = 'Invalid username or password';
+//       });
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: PreferredSize(
+//         preferredSize: const Size.fromHeight(120.0),
+//         child: AppBar(
+//           title: const Text('Welcome Back!'),
+//           toolbarHeight: 120.0,
+//           automaticallyImplyLeading: false,
+//           flexibleSpace: Container(),
+//           centerTitle: true,
+//           backgroundColor: Color(0xFFC0A172),
+//           elevation: 0,
+//           shape: const Border(
+//             bottom: BorderSide(width: 1, color: Colors.black12),
+//           ),
+//         ),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(12.0),
+//         child: Container(
+//           decoration: BoxDecoration(
+//             color: Colors.white, // Background color of the container
+//             borderRadius: BorderRadius.circular(
+//                 16.0), // Curved borders with a radius of 16
+//             boxShadow: [
+//               BoxShadow(
+//                 color: Colors.black.withValues(alpha: 0.1),
+//                 blurRadius: 10,
+//                 offset: Offset(0, 4), // Shadow effect
+//               ),
+//             ],
+//           ),
+//           child: Column(
+//             mainAxisAlignment:
+//                 MainAxisAlignment.start, // Align items at the top
+//             crossAxisAlignment:
+//                 CrossAxisAlignment.center, // Center horizontally
+//             children: [
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   Text(
+//                     "Sign In",
+//                     style: TextStyle(fontSize: 24),
+//                     textAlign: TextAlign.center,
+//                   ),
+//                 ],
+//               ),
+//               SizedBox(height: 32), // Space between the title and form
+//               Column(
+//                 mainAxisAlignment: MainAxisAlignment
+//                     .start, // Align items at the top of the form
+//                 crossAxisAlignment: CrossAxisAlignment
+//                     .stretch, // Stretch form fields to fill the width
+//                 children: [
+//                   TextField(
+//                     controller: _usernameController,
+//                     decoration: InputDecoration(
+//                       labelText: 'Username',
+//                       border: OutlineInputBorder(),
+//                     ),
+//                   ),
+//                   SizedBox(height: 16),
+//                   TextField(
+//                     controller: _passwordController,
+//                     decoration: InputDecoration(
+//                       labelText: 'Password',
+//                       border: OutlineInputBorder(),
+//                     ),
+//                     obscureText: true,
+//                   ),
+//                   SizedBox(height: 16),
+//                   if (_errorMessage.isNotEmpty)
+//                     Text(
+//                       _errorMessage,
+//                       style: TextStyle(color: Colors.red),
+//                     ),
+//                   SizedBox(height: 16),
+//                   ElevatedButton(
+//                     onPressed: _login,
+//                     child: Text('Sign In'),
+//                   ),
+//                 ],
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'welcome.dart';
 
@@ -32,42 +156,242 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Page'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _usernameController,
-              decoration: InputDecoration(
-                labelText: 'Username',
-                border: OutlineInputBorder(),
+      backgroundColor: Color(0xFFBFAB93), // Tan background color
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: [
+              // Welcome Text
+              Positioned(
+                top: 80,
+                left: 0,
+                right: 0,
+                child: Center(
+                  child: Text(
+                    'Welcome Back!',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
               ),
-            ),
-            SizedBox(height: 16),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
+
+              // White Container with Curved Top
+              Positioned(
+                top: 160,
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height - 160,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(30),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(height: 20),
+                        Text(
+                          'Sign In',
+                          style: TextStyle(
+                            fontSize: 28,
+                            color: Color(0xFFBFAB93),
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        SizedBox(height: 40),
+
+                        // Email TextField
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Color(0xFFE8E8E8),
+                            borderRadius: BorderRadius.circular(30),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.05),
+                                blurRadius: 5,
+                                offset: Offset(0, 3),
+                              ),
+                            ],
+                          ),
+                          child: TextField(
+                            controller: _usernameController,
+                            decoration: InputDecoration(
+                              hintText: 'Email',
+                              hintStyle: TextStyle(color: Colors.grey),
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.symmetric(
+                                  horizontal: 25, vertical: 20),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+
+                        // Password TextField
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Color(0xFFE8E8E8),
+                            borderRadius: BorderRadius.circular(30),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.05),
+                                blurRadius: 5,
+                                offset: Offset(0, 3),
+                              ),
+                            ],
+                          ),
+                          child: TextField(
+                            controller: _passwordController,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintText: 'Password',
+                              hintStyle: TextStyle(color: Colors.grey),
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.symmetric(
+                                  horizontal: 25, vertical: 20),
+                              suffixIcon: Padding(
+                                padding: EdgeInsets.only(right: 15),
+                                child: Icon(Icons.visibility_off,
+                                    color: Colors.grey),
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        if (_errorMessage.isNotEmpty)
+                          Padding(
+                            padding: EdgeInsets.only(top: 8),
+                            child: Text(
+                              _errorMessage,
+                              style: TextStyle(color: Colors.red),
+                            ),
+                          ),
+
+                        // Forget Password Link
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              'forget your password ?',
+                              style: TextStyle(
+                                color: Color(0xFFBFAB93),
+                                fontSize: 14,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+
+                        // Sign In Button
+                        Container(
+                          width: double.infinity,
+                          height: 55,
+                          child: ElevatedButton(
+                            onPressed: _login,
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Color(0xFFBFAB93),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              elevation: 5,
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'Sign In',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                SizedBox(width: 10),
+                                Icon(Icons.arrow_forward, color: Colors.white),
+                              ],
+                            ),
+                          ),
+                        ),
+
+                        SizedBox(height: 30),
+
+                        // Or Sign In With Text
+                        Center(
+                          child: Text(
+                            'Or Sign In With',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ),
+
+                        SizedBox(height: 20),
+
+                        // Google Sign In Button
+                        Center(
+                          child: Container(
+                            width: 60,
+                            height: 60,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.1),
+                                  blurRadius: 8,
+                                  offset: Offset(0, 3),
+                                ),
+                              ],
+                            ),
+                            child: Center(
+                              child: Text(
+                                'G',
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  color: Color(0xFFBFAB93),
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        SizedBox(height: 30),
+
+                        // Create Account Link
+                        Center(
+                          child: RichText(
+                            text: TextSpan(
+                              text: "Don't have an account? ",
+                              style: TextStyle(color: Colors.grey),
+                              children: [
+                                TextSpan(
+                                  text: 'Create One',
+                                  style: TextStyle(
+                                    color: Color(0xFFBFAB93),
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ),
-              obscureText: true,
-            ),
-            SizedBox(height: 16),
-            if (_errorMessage.isNotEmpty)
-              Text(
-                _errorMessage,
-                style: TextStyle(color: Colors.red),
-              ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: _login,
-              child: Text('Log In'),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
