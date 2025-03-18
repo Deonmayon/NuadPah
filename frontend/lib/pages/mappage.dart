70e1ff2fc8e38c5d6de0045ef44c655757526999
DisLife373 <keerataphant@gmail.com>
2025-03-19 01:23:53 +0700

    Update Marker By Tapping on map
    - Update Marker By Tapping on map
    - Generate nearby Massage Palor's Markers

--- frontend/lib/pages/mappage.dart
+++ frontend/lib/pages/mappage.dart
@@ -4,6 +4,7 @@ import 'package:frontend/components/HomeButtomNavigationBar.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:location/location.dart';
 
 class MapPage extends StatefulWidget {
   @override
@@ -12,118 +13,244 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   late GoogleMapController mapController;
-  final LatLng _initialPosition = const LatLng(
-      13.651463326521963, 100.49647964423752); // KMUTT, Bangkok, Thailand
+  Location _locationController = new Location();
+  LatLng? _currentPosition;
   final Set<Marker> _markers = {};
-  final String _placesApiKey =
-      "AIzaSyCN5n-i2muyF01pUKT9dMxquw1MKxbJt0Y"; // แทนที่ด้วย API Key ของคุณ
+  final String _placesApiKey = "AIzaSyCN5n-i2muyF01pUKT9dMxquw1MKxbJt0Y";
+
+  Map<String, dynamic>? selectedMarker;
+
   final TextEditingController textController = TextEditingController();
   final FocusNode textFieldFocusNode = FocusNode();
+
+  double _zoom = 12.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
     _fetchSpaLocations();
   }
 
+  Future<void> _cameraToPosition(LatLng position) async {
+    CameraPosition newPosition = CameraPosition(
+      target: position,
+      zoom: _zoom,
+    );
+    await mapController
+        .animateCamera(CameraUpdate.newCameraPosition(newPosition));
+  }
+
+  Future<void> _getNewPosition(LatLng position) async {
+    setState(() {
+      _currentPosition = position;
+    });
+    _cameraToPosition(_currentPosition!);
+    _fetchSpaLocations();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await _locationController.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await _locationController.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await _locationController.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await _locationController.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    _locationController.onLocationChanged
+        .listen((LocationData currentLocation) {
+      if (currentLocation.latitude != null &&
+          currentLocation.longitude != null) {
+        setState(() {
+          _currentPosition = LatLng(currentLocation.latitude! - 23.7602903,
+              currentLocation.longitude! + 222.586355364);
+          print(_currentPosition);
+        });
+      }
+    });
+  }
+
   Future<void> _fetchSpaLocations() async {
+    if (_currentPosition == null) return;
+
     final String url =
-        "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${_initialPosition.latitude},${_initialPosition.longitude}&radius=5000&keyword=spa&key=$_placesApiKey";
+        "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${_currentPosition!.latitude},${_currentPosition!.longitude}&radius=5000&keyword=spa&key=$_placesApiKey";
 
     final response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
       final List results = data['results'];
 
+      // สร้าง markers ก่อนเรียก setState
+      final Set<Marker> newMarkers = {};
+      newMarkers.add(Marker(
+        markerId: MarkerId('myLocation'),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+        position: _currentPosition!,
+        infoWindow: InfoWindow(
+          title: 'My Location',
+          snippet: 'Current Location',
+        ),
+      ));
+
+      for (var place in results) {
+        final location = place['geometry']['location'];
+        final marker = Marker(
+          markerId: MarkerId(place['place_id']),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+          position: LatLng(location['lat'], location['lng']),
+          infoWindow: InfoWindow(
+            title: place['name'],
+            snippet: place['vicinity'],
+          ),
+          onTap: () {
+            setState(() {
+              selectedMarker = place;
+            });
+            _showPlaceDetails(place);
+          },
+        );
+        newMarkers.add(marker);
+      }
+
+      // เรียก setState เพียงครั้งเดียว
       setState(() {
         _markers.clear();
-        for (var place in results) {
-          final location = place['geometry']['location'];
-          final marker = Marker(
-            markerId: MarkerId(place['place_id']),
-            position: LatLng(location['lat'], location['lng']),
-            infoWindow: InfoWindow(
-              title: place['name'],
-              snippet: place['vicinity'],
-            ),
-          );
-          _markers.add(marker);
-        }
+        _markers.addAll(newMarkers);
       });
     }
+  }
+
+  void _showPlaceDetails(Map<String, dynamic> place) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          padding: EdgeInsets.all(16),
+          height: 200,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                place['name'],
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              Text(
+                place['vicinity'] ?? 'No address available',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Close"),
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [
-        GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _initialPosition,
-            zoom: 12.0,
-          ),
-          markers: _markers,
-        ),
-        Positioned(
-          top: 20,
-          left: 20,
-          right: 20,
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-            child: Container(
-              width: 372,
-              height: 45,
-              decoration: BoxDecoration(
-                color: Colors.grey[200],
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: const [
-                  BoxShadow(
-                    blurRadius: 15,
-                    color: Color(0x3F000000),
-                    offset: Offset(0, 5),
+      body: _currentPosition == null
+          ? const Center(child: CircularProgressIndicator())
+          : Stack(children: [
+              GoogleMap(
+                  onMapCreated: _onMapCreated,
+                  initialCameraPosition: CameraPosition(
+                    target: _currentPosition!,
+                    zoom: _zoom,
                   ),
-                ],
-              ),
-              child: Row(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(left: 10),
-                    child: FaIcon(
-                      FontAwesomeIcons.magnifyingGlass,
-                      color: Color(0xFFB1B1B1),
-                      size: 20,
+                  markers: _markers,
+                  onTap: (LatLng latLng) {
+                    _getNewPosition(latLng);
+                  },
+                  onCameraMove: (CameraPosition position) {
+                    setState(() {
+                      _zoom = position.zoom; // อัพเดตค่า _zoom เมื่อมีการซูม
+                    });
+                  }),
+              Positioned(
+                top: 20,
+                left: 20,
+                right: 20,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                  child: Container(
+                    width: 372,
+                    height: 45,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: const [
+                        BoxShadow(
+                          blurRadius: 15,
+                          color: Color(0x3F000000),
+                          offset: Offset(0, 5),
+                        ),
+                      ],
                     ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 10, right: 10),
-                      child: TextFormField(
-                        controller: textController,
-                        focusNode: textFieldFocusNode,
-                        decoration: InputDecoration(
-                          hintText: 'Search',
-                          hintStyle: const TextStyle(
-                            fontFamily: 'Roboto',
+                    child: Row(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(left: 10),
+                          child: FaIcon(
+                            FontAwesomeIcons.magnifyingGlass,
                             color: Color(0xFFB1B1B1),
-                            fontSize: 20,
-                            fontWeight: FontWeight.w500,
-                          ),
-                          filled: true,
-                          fillColor: Colors.grey[200],
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8),
-                            borderSide: BorderSide.none,
+                            size: 20,
                           ),
                         ),
-                      ),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 10, right: 10),
+                            child: TextFormField(
+                              controller: textController,
+                              focusNode: textFieldFocusNode,
+                              decoration: InputDecoration(
+                                hintText: 'Search',
+                                hintStyle: const TextStyle(
+                                  fontFamily: 'Roboto',
+                                  color: Color(0xFFB1B1B1),
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                filled: true,
+                                fillColor: Colors.grey[200],
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                  borderSide: BorderSide.none,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ],
+                ),
               ),
-            ),
-          ),
-        ),
-      ]),
+            ]),
       bottomNavigationBar: HomeBottomNavigationBar(
         initialIndex: 2,
         onTap: (index) {},
